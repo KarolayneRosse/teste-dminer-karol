e3044c46943e30f94f91bbf22580065ee950dd5b
Usuario de Teste <UsuarioTeste@IMAC008.local>
2021-04-16 11:14:54 -0300

    Response direcionativo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,11 @@
       <b-form>
         <b-form-group label="Nome">
           <b-input-group>
-            <b-form-input type="text" v-model="form.userName" class="border-dark"></b-form-input>
+            <b-form-input
+              type="text"
+              v-model="form.userName"
+              class="border-dark"
+            ></b-form-input>
           </b-input-group>
         </b-form-group>
 
@@ -17,7 +21,7 @@
           </b-input-group>
         </b-form-group>
       </b-form>
-      <b-button @click="submit()">{{ btnReq }}</b-button> 
+      <b-button @click="submit()">{{ btnReq }}</b-button>
     </b-card>
   </div>
 </template>
@@ -37,21 +41,24 @@ export default {
     submit() {
       if (!this.load) {
         this.load = true;
-        this.btnReq = 'Enviando...'
+        this.btnReq = "Enviando...";
 
         this.$http
           .post("https://www.dminerweb.com.br:8553/api/auth", this.form)
-          .then((res) => {
-              this.btnReq = 'Enviar'
-            console.log(res.data);
-            this.load = false
-            //   this.$store.state.loggedIn = true;
-            //this.$store.state.user = res.data
-            //   this.$router.push("/");
-          }, error =>{
-              this.btnReq = 'Enviar'
+          .then(
+            (res) => {
+              this.btnReq = "Enviar";
+              console.log(res.data);
+              this.load = false;
+              this.$store.state.loggedIn = true;
+              this.$store.state.user = res.data;
+              this.$router.push("/");
+            },
+            (error) => {
+              this.btnReq = "Enviar";
               console.error(error.data);
-          });
+            }
+          );
       }
     },
   },
